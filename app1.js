7ce0d1536af2f8c50902d4d70f875c27e5dfe5e9
75310shubham <118808458+75310shubham@users.noreply.github.com>
2022-11-25 16:26:47 +0530

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,22 @@
+// let paragraphElement= document.getElementById('button');
+// let paragraphElement=document.body.firstElementChild;
+let paragraphElement = document.body.querySelector("p");
+
+// console.log(paragraphElement);
+function changeParagraphText() {
+  paragraphElement.textContent = "Clicked";
+  console.log("paragraph clicked");
+}
+
+paragraphElement.addEventListener("click", changeParagraphText);
+
+let inputTextElement = document.getElementById("inputText");
+function changeInputText(eventObject) {
+  let valueInput = inputTextElement.value;
+  // let valueInput=eventObject.target.value;
+  // let valueInput= event.data;
+
+  console.log(valueInput);
+  console.log(eventObject);
+}
+inputTextElement.addEventListener("input", changeInputText);
