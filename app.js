21f61fb4affc8f316628f429dfb92675a7926100
75310shubham <118808458+75310shubham@users.noreply.github.com>
2022-11-29 14:49:35 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,49 @@
-const productNameInputElement = document.getElementById("product-name");
-const remainingCharsElement= document.getElementById("remaining-char");
+// console.log(window);
+// alert('Hi');
+// window.alert('Hi2');
 
-function updateRemainingCharacter(event){
-    const enterdText=event.target.value;
-    console.log(enterdText);
-    const enterdTextLength= enterdText.length;
-    console.log(enterdTextLength);
-    const remainingCharacters= 60-enterdTextLength;
-    remainingCharsElement.textContent = remainingCharacters;
-}
+// console.log(document);
+//To check HTML code in console.
+// console.dir(document);
+//To check all the parsed code, which is DOM.
 
-productNameInputElement.addEventListener("input",updateRemainingCharacter);
+// document.body.children[1].children[0].href = "https://amazon.com";
+// This is drilling a document method
 
-const spanElement= document.getElementById('remaining-char');
+// By Element ID
+// let anchorElement = document.getElementById('external-link');
 
-// spanElement.style.color='rgb(255,0,0)';
-spanElement.className='warning';
-// spanElement.classList.add('warning');
+// anchorElement.href= 'https://google.com';
+// console.dir(anchorElement);
+
+// By Query Selector
+// let anchorElement= document.querySelector('h1');
+// console.dir(anchorElement);
+// anchorElement.href='https://google.com';
+
+//ADD AN ELEMENT
+//1. नया element बनाना है, जिसको हम append करना चाहते है
+// let newAnchorElement = document.createElement("a");
+// newAnchorElement.href = "https://google.com";
+// newAnchorElement.textContent = "This leads to google";
+// //2. एक और नया element बनाना है,जिससे हमे उसके parent का access मिल जाए
+// let firstParagraph = document.querySelector("p");
+// //3. अब उस नए element को parent element append कर देंगे
+// firstParagraph.append(newAnchorElement);
+
+// //REMOVE AN ELEMENT
+// //1. उस element select करे, जिसे remove करना चाहते है
+// let firstH1Element = document.querySelector("h1");
+// //2. Remove करे
+// firstH1Element.remove();
+
+// // //MOVE AN ELEMENT
+// firstParagraph.parentElement.append(firstParagraph);
+
+// // innerHTML
+// console.log(firstParagraph.textContent);
+// console.log(firstParagraph.innerHTML);
+
+// firstParagraph.textContent = "Changed by <strong>textContent</strong>";
+
+// firstParagraph.innerHTML = "Hello,<strong> You</strong>";
